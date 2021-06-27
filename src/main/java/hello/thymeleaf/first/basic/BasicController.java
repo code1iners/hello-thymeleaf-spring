a1aa5ddcf7ea2fdfbda48b7fb9c1a8dfa50c48a5
Codeliner <codeliner96@gmail.com>
2021-06-27 16:22:02 +0900

    [001] text.

---
+++ src/main/java/hello/thymeleaf/first/basic/BasicController.java
@@ -0,0 +1,23 @@
+package hello.thymeleaf.first.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "basic")
+public class BasicController {
+    
+    @GetMapping(value = "text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-basic";
+    }
+
+    @GetMapping(value = "text-unescaped")
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-unescaped";
+    }
+}
