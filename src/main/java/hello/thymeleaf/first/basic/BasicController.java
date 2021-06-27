ab2961549e74ed0a69bfcf1312c67a1290abbe21
Codeliner <codeliner96@gmail.com>
2021-06-27 16:40:00 +0900

    [002] variable SpringEL expression.

--- src/main/java/hello/thymeleaf/first/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/first/basic/BasicController.java
@@ -1,23 +1,71 @@
 package hello.thymeleaf.first.basic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.Data;
+
 @Controller
 @RequestMapping(value = "basic")
 public class BasicController {
     
+    /**
+     * [001] Text (escaped) - default.
+     */
     @GetMapping(value = "text-basic")
     public String textBasic(Model model) {
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-basic";
     }
 
+    /**
+     * [001] Text (unescaped).
+     */
     @GetMapping(value = "text-unescaped")
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-unescaped";
+    }
+
+    /**
+     * [002] variables SpringEL expression.
+     */
+    @GetMapping(value = "variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> users = new ArrayList<>();
+        users.add(userA);
+        users.add(userB);
+
+        Map<String, User> userMap = new HashMap<>();
+        userMap.put("userA", userA);
+        userMap.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", users);
+        model.addAttribute("userMap", userMap);
+
+        return "basic/variable";
+    }
+
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
